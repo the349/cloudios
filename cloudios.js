8babb2a6b3aaddf2e3c1c50d977e2c173c9def09
the349 <josephmalone25@gmail.com>
2018-03-18 23:18:08 -0400

    Add files via upload

---
+++ cloudios.js
@@ -0,0 +1,25 @@
+/*
+* Cloudios v0.1
+* This is the Pre-releace
+* By The349
+* Goto http://mrjay.oyosite.com/cloudios.html for build and test info
+*/
+var cloudiosVersion = "v0.1"
+function cloudios(i) {
+    self.element = document.querySelector(i)
+    self.html = function (i) {
+        if(!i) {return self.element}
+        if(i) {self.element.innerHTML = i}
+        return self
+    }
+    self.attr = function (name, value) {
+        if(!value) {self.element.getAttribute(name)}
+        if(value) {self.element.setAttribute(name, value)}
+        return self
+    }
+    self.on = function (type, callback) {
+        self.element["on" + type] = callback
+        return self
+    }
+    return self
+}
